59c7b8830c993985c81e97d4f63ae4f1dcf6d3e8
tdinu <dinutitiana@yahoo.com>
2024-11-10 17:53:41 +0100

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,21 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
-  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/src/jest.setup.ts"],
   transform: {
-    '^.+.tsx?$': ['ts-jest', {}],
+    "^.+.tsx?$": ["ts-jest", {}],
   },
   globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.jest.json',
+    "ts-jest": {
+      tsconfig: "./tsconfig.jest.json",
+    },
+  },
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
   },
 };
