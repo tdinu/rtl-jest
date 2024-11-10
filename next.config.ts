fb0532537bbadb2265a0b9282a0904c90d120326
tdinu <dinutitiana@yahoo.com>
2024-11-10 12:14:57 +0100

    initial setup

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
-import type { NextConfig } from "next";
+import path from 'path';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.join(__dirname, 'src');
+    return config;
+  },
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx'], // Add this if you use TypeScript or other extensions
 };
 
 export default nextConfig;
