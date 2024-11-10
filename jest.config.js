b682eb34a8e73ce000ff218707864cb471b96504
tdinu <dinutitiana@yahoo.com>
2024-11-10 13:01:13 +0100

    setup for dom test

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "node",
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/jest.setup.ts'],
   transform: {
-    "^.+.tsx?$": ["ts-jest",{}],
+    '^.+.tsx?$': ['ts-jest', {}],
+  },
+  globals: {
+    'ts-jest': {
+      tsconfig: './tsconfig.jest.json',
+    },
   },
 };
